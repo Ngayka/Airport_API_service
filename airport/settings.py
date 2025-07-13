2ce91be36e7b6a5d89cdb67f8749b37830a98344
Ngayka <ngayka@gmail.com>
2025-07-13 09:02:31 +0300

    Add secret-key to .env

--- airport/settings.py
+++ airport/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3#6ru8vojyedxs$tvwmhan)l-s*4=q6_(n=8xw-225+2hkghfo"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
